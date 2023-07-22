782a0cad3e42cf5a76904c953ed8a9ec2c80699f
Ivan <ivan.zf.netc@gmail.com>
2023-07-22 21:12:44 +0300

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+
+interface ListItem {
+  text: string;
+  height: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Angular-drag';
+
+  todo: ListItem[] = [
+    { text: '', height: 140 },
+    { text: 'Drag me!', height: 140 },
+    { text: '', height: 80 },
+  ];
+
+  done: ListItem[] = [
+    {text: '', height: 80},
+    {text: '', height: 80},
+    {text: '', height: 140},
+    {text: '', height: 180},
+  ];
+
+  done2: ListItem[] = [
+    {text: '', height: 180},
+  ];
+
+  drop(event: CdkDragDrop<ListItem[]>) {
+    const prevContainer = event.previousContainer;
+    const currContainer = event.container;
+
+    if (prevContainer === currContainer) {
+      moveItemInArray(currContainer.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        prevContainer.data,
+        currContainer.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NewComponent } from './new/new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
